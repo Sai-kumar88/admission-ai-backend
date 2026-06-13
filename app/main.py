59bfa5f30c811887b0435793bc8fe6d80c696e20
Sai-kumar88 <Saikumar@vapstech.com>
2026-06-13 16:33:40 +0530

    Added CORS support

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import trends
 
 app = FastAPI(
     title="Admission Analytics Dashboard API",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for testing
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(trends.router)
